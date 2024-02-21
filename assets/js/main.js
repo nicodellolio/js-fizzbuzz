ca8f1c62d84c23ffb81af512410c61e535c17ef0
nicodellolio <nico.dellolio18@gmail.com>
2024-02-21 17:14:12 +0100

    here is my issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ TOOLS
 // # BONUS 
 // Inseriamo nel DOM un container ed attraverso la funzione append[()] un elemento html con il numero o la stringa corretta da mostrare
 
-//Creiamo la variabile per 'innerHTML'
+// Creiamo la variabile per 'innerHTML'
 const mainElement = document.querySelector('main')
 console.log(mainElement);
 
@@ -27,30 +27,38 @@ console.log(markupContainer);
 mainElement.innerHTML = markupContainer;
 
 
-
-
 //Creiamo il ciclo da 1 a 100 con incremento
 for (let i = 1; i <= 100; i++) {
 
-    // creiamo le condizioni che soddisfino il nostro ciclo. La prima utilizzando l'operatore remainder per confermare che 'i' ogni volta che viene moltiplicato per 3 sia identico a 0
-    if (i % 3 === 0) {
-        console.log("Fizz");
+    // creiamo le condizioni che soddisfino il nostro ciclo.
+    //La prima utilizzando l'operatore remainder per confermare che 'i' ogni volta che viene moltiplicato per 3 sia identico a 0 e che 'i' ogni volta che viene moltiplicato per 5 sia identico a 0, utilizzando l'operatore logico AND per confermare entrambe le condizioni
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
 
     // altrimenti la seconda condizione, utilizzando l'operatore remainder per confermare che 'i' ogni volta che viene moltiplicato per 5 sia identico a 0
-    } else if (i % 5 === 0) {
-        console.log("Buzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
 
-    // altrimenti in questo altro caso utilizziamo anche l'operatore logico AND per confermare entrambe le condizioni
-    } else if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FIzzBuzz");
+    // altrimenti in questo altro caso utilizziamo anche l'operatore logico AND per confermare che 'i' ogni volta che viene moltiplicato per 3 sia identico a 0
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
 
     // infine, se il numero iterato non soddisfa nessuna delle condizioni precedenti, semplicemnte viene stampato il numero corrispondente nell'ordine 
     } else {
       console.log(i);
     }
 
-    
+
+  // Creiamo un nuovo elemento div
+  const div = document.createElement(div);
+
+  const content = document.createTextNode(`${i}`);
+
+  div.appendChild(content);
+
+  container.appendChild(div);
 }
+
 
 
 
--- readme.md
+++ readme.md
@@ -19,4 +19,4 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 # BONUS 1:
 Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 # BONUS 2:
-Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
