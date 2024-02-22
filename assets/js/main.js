064aef1e8649500b8f671ee1d3e7c47bafbb7e0f
nicodellolio <nico.dellolio18@gmail.com>
2024-02-22 11:31:49 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,50 +14,43 @@ TOOLS
 */
 
 
+// Dichiariamo la costante che chiama il conntainer nel DOM
+const container = document.getElementById("container");
 
-const markupContainer = document.getElementById(container);
-//Creiamo il ciclo da 1 a 100 con incremento
 for (let i = 1; i <= 100; i++) {
-
-    // creiamo le condizioni che soddisfino il nostro ciclo.
-    //La prima utilizzando l'operatore remainder per confermare che 'i' ogni volta che viene moltiplicato per 3 sia identico a 0 e che 'i' ogni volta che viene moltiplicato per 5 sia identico a 0, utilizzando l'operatore logico AND per confermare entrambe le condizioni
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-
+  // creiamo un div di cui all'interno inserire l'oggetto iterato dal ciclo
+  let markupElement = document.createElement('div');
+  // creiamo le condizioni che soddisfino il nostro ciclo.
+  //La prima utilizzando l'operatore remainder per confermare che 'i' ogni volta che viene moltiplicato per 3 sia identico a 0 e che 'i' ogni volta che viene moltiplicato per 5 sia identico a 0, utilizzando l'operatore logico AND per confermare entrambe le condizioni
+  if (i % 3 === 0 && i % 5 === 0) {
+    markupElement.append("FizzBuzz");
     // altrimenti la seconda condizione, utilizzando l'operatore remainder per confermare che 'i' ogni volta che viene moltiplicato per 5 sia identico a 0
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-
+  } else if (i % 3 === 0) {
+    markupElement.append("Fizz");
+    
     // altrimenti in questo altro caso utilizziamo anche l'operatore logico AND per confermare che 'i' ogni volta che viene moltiplicato per 3 sia identico a 0
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
+  } else if (i % 5 === 0) {
+    markupElement.append("Buzz");
 
     // infine, se il numero iterato non soddisfa nessuna delle condizioni precedenti, semplicemnte viene stampato il numero corrispondente nell'ordine 
-    } else {
-      console.log(i);
-    }
+  } else {
+    markupElement.append(i);
+  }
 
-    console.log(i);
-    const markupBoxItem = `<div class="box">${i}</div>`;
-    console.log(markupBoxItem);
-
-    markupContainer.innerHTML += markupBoxItem;
-
+  container.append(markupElement);
 }
 // Creiamo un nuovo elemento div
-//     const div = document.createElement("div");
- 
-//   //const content = document.createTextNode(`${i}`);
-//     div.innerText = i;
-
-//     document.getElementById(container).appendChild(div);
 
 
 
+    
 
 
 
-
-
+    
+    
+    
+    
+    
 
 
